b848f813a325af92c9527be88e76c8d21412c34d
An-MinSu <dmdkrgkrrydp@naver.com>
2022-03-01 17:05:17 +0900

    add init,database code

--- public/js/register.js
+++ public/js/register.js
@@ -1,9 +1,9 @@
-import { app } from "../js/firebase.js";
+import { app, firebaseConfig } from "../js/firebase.js";
 import { getDatabase, ref, set, push, child, get, update, remove } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
+firebase.initializeApp(firebaseConfig);
 
-var Auth = firebase.auth();
-var Fdatabase = firebase.getDatabase;
+const Auth = firebase.auth();
 var userInfo;
 
 $(document).ready(function(){
@@ -17,33 +17,27 @@ $(document).ready(function(){
     }
     else{
       Auth.createUserWithEmailAndPassword(email, pwd2).then(function(user) {
-    
-        userInfo = user;
-        //logUser();
-        alert("회원가입 완료");
-        window.location.href = "../html/login.html"
+
+        
+        logUser(user);
     
     }, function(error) {
         //에러가 발생했을 때 
         var errorCode = error.code;
         var errorMessage = error.message;
         alert(errorMessage);
-    });
+      });
     }
-    });
+  });
 });
 
-function logUser(){
-  var ref = Fdatabase.ref("users/"+userInfo.uid); //저장될 곳을 users라는 부모 키를 레퍼런스로 지정.
-  
-  //저장 형식
-  var obj = {
-      email: email
-  };
+function logUser(info){
 
-  ref.set(obj); // 고유한 자식 키가 하나 생셩이 되면서 json 삽입
-  alert("가입성공");
-
-  //메인 페이지로 이동시키고 세션 저장시키기
+  const db = getDatabase();
+  set(ref(db,'users/'+info.uid),{
+    email:email
+  })
+  console.log(info.uid);
+  alert("회원가입 완료");
   window.location.href = "../html/login.html"
 }
